1eae0e728b4fe893a9428cbac7acbdc0795735d3
Jan Eisenhauer <jan.eisenhauer@rwu.de>
2021-02-05 15:58:53 +0100

    Initialize goal pool in robot navigation

--- je_171731_prj/src/robot_navigation.py
+++ je_171731_prj/src/robot_navigation.py
@@ -1,9 +1,11 @@
+from goal import GoalPool
 from robot_control import RobotControl
 
 
 class RobotNavigation:
     def __init__(self):
         self._robot_control = RobotControl()
+        self._goal_pool = GoalPool()
 
     def update(self):  # type: () -> None
         pass

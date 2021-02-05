d3778f017a7f7084d5180531cfdbcbaaf6c43724
Jan Eisenhauer <jan.eisenhauer@rwu.de>
2021-02-05 19:18:49 +0100

    Select goal depending on distance and reward
    Solves #15

--- je_171731_prj/src/goal_selector.py
+++ je_171731_prj/src/goal_selector.py
@@ -6,12 +6,13 @@ from robot_state import RobotState
 class GoalSelector:
     def select_goal(self, goals, robot_state):
         # type: (list, RobotState) -> Goal
-        min_distance_sqrt = None
+        min_distance_reward = None
         nearest_goal = None
         for goal in list(goals):
             distance_sqrt = goal.distance_sqrt(robot_state.exact_position)
-            if min_distance_sqrt is None or min_distance_sqrt > distance_sqrt:
-                min_distance_sqrt = distance_sqrt
+            distance_reward = distance_sqrt * (1.0 / goal.reward)
+            if min_distance_reward is None or min_distance_reward > distance_reward:
+                min_distance_reward = distance_reward
                 nearest_goal = goal
 
         return nearest_goal
